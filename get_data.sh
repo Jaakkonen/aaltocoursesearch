e89b50a6b5c1a73f321b2da0e3130b20484c5e9f
Jaakko Sirén <jaakko.s@iki.fi>
2023-04-05 14:23:42 +0300

    Delete old course data gathering script

--- get_data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ -z "$COURSE_API_TOKEN" ]]; then
-    echo "Please set COURSE_API_TOKEN to one obtained from 3scale.ocp.aalto.fi SISU api."
-    exit
-fi
-curl "https://course.api.aalto.fi/api/sisu/v1/courseunitrealisations?USER_KEY=$COURSE_API_TOKEN" > courseunitrealizations.json
